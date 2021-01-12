335b956ab21b5112c9dbb076d976a2136808a227
cblancofidalgo <cblancofidalgo@danielcastelao.org>
2021-01-12 11:29:34 +0100

    bajamos lineas para que nos deje hacer merge

--- src/com/cod/Main.java
+++ src/com/cod/Main.java
@@ -3,6 +3,8 @@ package com.cod;
 public class Main {
 
     public static void main(String[] args) {
+
+
         System.out.println("salgo de la rama principal");
         System.out.println("voy a la bugFix");
     }
