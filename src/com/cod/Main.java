72736ef780137738bde41f6e9c31dd9dc51c170f
cblancofidalgo <cblancofidalgo@danielcastelao.org>
2021-01-12 10:59:37 +0100

    nueva rama bugFix

--- src/com/cod/Main.java
+++ src/com/cod/Main.java
@@ -3,6 +3,7 @@ package com.cod;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("rama principal");
+        System.out.println("salgo de la rama principal");
+        System.out.println("voy a la bugFix");
     }
 }
